d604d31de71ef45b2dad2b02c4afdb3a9fc9beea
amr mustafa <amrmustafa6051@gmail.com>
2024-12-22 10:36:15 +0200

    make app colors abstract class

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-class AppColors {
+abstract class AppColors {
   static Color primaryColor = Color(0xffE2BE7F);
   static Color secondaryColor = Color(0xff202020);
   static Color white = Color(0xffFFFFFF);
